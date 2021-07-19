9ac8517ed16240298d89cb55cf20328fced95856
Andrew Clark <clarkandrew10@gmail.com>
2021-07-19 13:42:00 -0400

    Final Project

---
+++ main.js
@@ -0,0 +1,36 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("carousel-slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "grid";
+} 
+
+window.addEventListener('keydown', (event)=>{
+  if(event.key === 'ArrowLeft'){plusSlides(-1)}
+  if(event.key === 'ArrowRight'){plusSlides(1)}
+})
+
+function responseNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
